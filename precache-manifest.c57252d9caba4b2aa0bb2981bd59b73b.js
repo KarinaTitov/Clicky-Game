cddb6da421b15670134e03cf2552a0d64f6cce2b
Karina Titov <karinasul1995@gmail.com>
2020-06-30 21:43:01 -0500

    Updates

---
+++ precache-manifest.c57252d9caba4b2aa0bb2981bd59b73b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fcc3a7b0634a851c440afa84eb142c",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "a424d24d7d45243a252f",
+    "url": "/Clicky-Game/static/css/main.a9e5dddc.chunk.css"
+  },
+  {
+    "revision": "366154b1789333e3a261",
+    "url": "/Clicky-Game/static/js/2.086dc408.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.086dc408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a424d24d7d45243a252f",
+    "url": "/Clicky-Game/static/js/main.b054b5f5.chunk.js"
+  },
+  {
+    "revision": "7adb76f8cdbae61dff35",
+    "url": "/Clicky-Game/static/js/runtime-main.c09312a5.js"
+  }
+]);
